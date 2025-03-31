61152dfaec10e1d741aa0147f64585a88e89cf0c
rmmagee2 <130113674+rmmagee2@users.noreply.github.com>
2025-03-31 09:33:04 -0400

    Update adc.h

--- adc.h
+++ adc.h
@@ -10,4 +10,20 @@ extern volatile unsigned int adc_values[3];
 void Init_ADC(void);
 void Start_ADC(uint8_t channel);
 
+#define MODE_THRESHOLD_1 500
+#define MODE_THRESHOLD_2 1500
+#define MODE_THRESHOLD_3 2500
+#define MODE_THRESHOLD_4 3500
+
+typedef enum {
+    MODE_NONE,
+    MODE_START,
+    MODE_STOP,
+    MODE_MANUAL,
+    MODE_AUTO
+} Menu_Mode;
+
+Menu_Mode Thumbwheel_GetMode(void);
+void Thumbwheel_Menu(void);
+
 #endif /* ADC_H_ */
