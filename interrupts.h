3d3767a49fc9f6fa72a4b4cf6abd61f362d16981
rmmagee2 <130113674+rmmagee2@users.noreply.github.com>
2025-03-31 09:08:40 -0400

    Update interrupts.h

--- interrupts.h
+++ interrupts.h
@@ -3,5 +3,7 @@
 
 __interrupt void Timer0_B0_ISR(void);
 __interrupt void TIMER0_B1_ISR(void);
+__interrupt void USCI_A0_ISR(void);
+__interrupt void USCI_A1_ISR(void);
 
 #endif /* INTERRUPTS_H_ */
