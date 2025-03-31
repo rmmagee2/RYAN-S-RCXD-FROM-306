c16fa5b183adc862b1a97487a15c31c496fe4ff2
rmmagee2 <130113674+rmmagee2@users.noreply.github.com>
2025-03-31 09:03:59 -0400

    Update uart_comm.h

--- uart_comm.h
+++ uart_comm.h
@@ -1,1 +1,18 @@
+#ifndef UART_H_
+#define UART_H_
 
+#define UART_BUFFER_SIZE     64
+#define PROCESS_BUFFER_SIZE  32
+
+// UART Channels
+#define UART_PC     0
+#define UART_IOT    1
+
+void Init_UART(void);
+void UART_Process(void);
+
+uint8_t UART_CheckForCommand(uint8_t channel);
+char* UART_GetCommand(uint8_t channel);
+void UART_SendString(uint8_t channel, const char* str);
+
+#endif // UART_H_
