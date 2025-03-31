84a07ffe3c6e7bda00036845afd3d6c4b7ea2a0f
rmmagee2 <130113674+rmmagee2@users.noreply.github.com>
2025-03-31 09:41:42 -0400

    Update timers.h

--- timers.h
+++ timers.h
@@ -1,5 +1,11 @@
 #ifndef TIMERS_H_
 #define TIMERS_H_
+#include <stdint.h>
+
+extern volatile uint8_t timer_done;
+
+void Init_Timer_B0(void);
+void Start_Delay(uint16_t ms);  // millisecond timer trigger
 
 void Init_Clocks(void);
 void Init_Timer_B0(void);
