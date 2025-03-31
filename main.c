916f77aac0acaf4909d3ed2fbeb22d05763021ba
rmmagee2 <130113674+rmmagee2@users.noreply.github.com>
2025-03-31 09:06:52 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
+UART_Process();
+
+if(UART_CheckForCommand(UART_IOT)) {
+    char* cmd = UART_GetCommand(UART_IOT);
+    if(strcmp(cmd, "start") == 0) {
+        // Start car
+    }
+}
+
+if(UART_CheckForCommand(UART_PC)) {
+    char* cmd = UART_GetCommand(UART_PC);
+    if(strcmp(cmd, "debug") == 0) {
+        UART_SendString(UART_PC, "Debug info here\n");
+    }
+}
 
