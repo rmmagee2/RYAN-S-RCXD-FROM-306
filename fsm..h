85134dbc912b6a1c1c4743dcfe13b7439bd86efd
rmmagee2 <130113674+rmmagee2@users.noreply.github.com>
2025-03-31 09:32:08 -0400

    Update and rename state_machine.h to fsm..h

---
+++ fsm..h
@@ -0,0 +1,23 @@
+// fsm.h
+#ifndef FSM_H
+#define FSM_H
+
+typedef enum {
+    STATE_IDLE,
+    STATE_THUMBWHEEL_MENU,
+    STATE_CONFIGURE_WHEELS,
+    STATE_FORWARD_START,
+    STATE_FORWARD_ADJUST,
+    STATE_REVERSE_START,
+    STATE_REVERSE_ADJUST,
+    STATE_INITIATE_STOP,
+    STATE_STOP,
+    STATE_PROCESS_COMMAND
+} FSM_State;
+
+void FSM_Init(void);
+void FSM_Process(void);
+void Set_Next_State(FSM_State new_state);
+
+#endif
+
