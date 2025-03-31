102cc473235ef6b262b4e45446c232f3e721256a
rmmagee2 <130113674+rmmagee2@users.noreply.github.com>
2025-03-31 09:19:04 -0400

    Update sensors.h

--- sensors.h
+++ sensors.h
@@ -3,12 +3,15 @@
 
 #include <stdint.h>
 
-#define BLACK_THRESHOLD    500  // Tune this based on surface testing
+#define BLACK_THRESHOLD    500  // Tune this value during calibration
 
 void Init_Sensors(void);
+void Update_Sensors(void);
 
 uint8_t Is_Black_Left(void);
 uint8_t Is_Black_Right(void);
+uint8_t Detect_Line_Center(void);
+
 uint16_t Get_Thumbwheel_PWM(void);
 
 #endif // SENSORS_H_
