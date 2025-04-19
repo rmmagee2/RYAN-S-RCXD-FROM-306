2ea0bae3aa06d457406d71e19c3e4d150558afb4
rmmagee2 <130113674+rmmagee2@users.noreply.github.com>
2025-04-19 18:38:49 -0400

    Update timers.h

--- timers.h
+++ timers.h
@@ -3,14 +3,15 @@
 #include <stdint.h>
 
 extern volatile uint8_t timer_done;
+extern volatile uint8_t timer_event_flag = 0;
+extern volatile uint8_t timer_event_flag; // in header
+volatile uint8_t timer_event_flag = 0;    // in .c file only
 
-void Init_Timer_B0(void);
 void Start_Delay(uint16_t ms);  // millisecond timer trigger
 
 void Init_Clocks(void);
 void Init_Timer_B0(void);
 void Init_Timer_B3(void);
-volatile uint8_t timer_event_flag = 0;
 
 #endif // TIMERS_H_
 
