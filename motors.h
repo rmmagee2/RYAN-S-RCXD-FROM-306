08cbe8bcf80f5637a5d08dd9092366df8ea2051a
rmmagee2 <130113674+rmmagee2@users.noreply.github.com>
2025-04-19 17:51:41 -0400

    Update motors.h

--- motors.h
+++ motors.h
@@ -1,17 +1,24 @@
 #ifndef MOTORS_H_
 #define MOTORS_H_
 
+#include <stdint.h>
+
 #define FULL_SPEED   1000
 #define HALF_SPEED    500
 #define OFF             0
 
 void Init_Motors(void);
 
-void Drive_Forward(unsigned int speed);
-void Drive_Reverse(unsigned int speed);
-void Turn_Left(unsigned int speed);
-void Turn_Right(unsigned int speed);
-void Stop_Motors(void);
+// FSM-level state functions
+void Configure_Wheels(void);
+
+void Move_Forward_Start(void);
+void Adjust_Forward(void);
+
+void Move_Reverse_Start(void);
+void Adjust_Reverse(void);
+
+void Stop_Movement_Init(void);
+void Stop_Movement_Final(void);
 
 #endif // MOTORS_H_
-
