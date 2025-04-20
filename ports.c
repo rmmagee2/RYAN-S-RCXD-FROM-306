0929f0216e9aa78d9e8147e00f0131630d1937ab
rmmagee2 <130113674+rmmagee2@users.noreply.github.com>
2025-04-20 10:29:48 -0400

    Update ports.c

--- ports.c
+++ ports.c
@@ -6,11 +6,9 @@ void Init_Ports(void) {
     P1OUT &= ~RED_LED;
     P1DIR |= RED_LED;
 
-    // ADC Inputs: IR L, IR R, Thumbwheel
     P1SEL0 |= IR_SENSOR_LEFT | IR_SENSOR_RIGHT | THUMBWHEEL;
     P1SEL1 |= IR_SENSOR_LEFT | IR_SENSOR_RIGHT | THUMBWHEEL;
 
-    // UART0 TX/RX for PC Debug
     P1SEL0 |= UART0_TX | UART0_RX;
     P1SEL1 &= ~(UART0_TX | UART0_RX);
 
@@ -18,19 +16,19 @@ void Init_Ports(void) {
     P2OUT &= ~IR_EMITTER;
     P2DIR |= IR_EMITTER;
 
-    P2OUT &= ~IOT_RED_LED;
-    P2DIR |= IOT_RED_LED;
+    P2OUT &= ~IOT_LED_RED;
+    P2DIR |= IOT_LED_RED;
 
-    P2OUT |= DAC_ENABLE;    // Power to DAC on
+    P2OUT |= DAC_ENABLE;
     P2DIR |= DAC_ENABLE;
 
-    P2DIR &= ~SW2;          // Optional input
+    P2DIR &= ~SW2;
     P2REN |= SW2;
     P2OUT |= SW2;
 
     // --- PORT 3 ---
-    P3OUT &= ~(IOT_GRN_LED | IOT_PWR_EN);
-    P3DIR |= IOT_GRN_LED | IOT_PWR_EN;
+    P3OUT &= ~(IOT_LED_GRN | IOT_PWR_EN);
+    P3DIR |= IOT_LED_GRN | IOT_PWR_EN;
 
     // --- PORT 4 ---
     P4OUT &= ~(LCD_RESET | LCD_CS);
@@ -39,22 +37,21 @@ void Init_Ports(void) {
     P4DIR &= ~SW1;
     P4REN |= SW1;
     P4OUT |= SW1;
-    P4IES |= SW1;       // High to low
+    P4IES |= SW1;
     P4IFG &= ~SW1;
     P4IE  |= SW1;
 
-    // UART1 & LCD SPI Setup
-    P4SEL0 |= UART1_TX | UART1_RX | LCD_SCK | LCD_SIMO | LCD_SOMI;
-    P4SEL1 &= ~(UART1_TX | UART1_RX | LCD_SCK | LCD_SIMO | LCD_SOMI);
+    P4SEL0 |= UART1_TX | UART1_RX | LCD_CLK | LCD_SIMO | LCD_SOMI;
+    P4SEL1 &= ~(UART1_TX | UART1_RX | LCD_CLK | LCD_SIMO | LCD_SOMI);
 
     // --- PORT 6 ---
     P6OUT |= LCD_BACKLIGHT;
     P6DIR |= LCD_BACKLIGHT;
 
-    // Motor PWM Direction Pins (All OFF initially)
-    P6OUT &= ~(R_FORWARD | R_REVERSE | L_FORWARD | L_REVERSE);
-    P6DIR |= (R_FORWARD | R_REVERSE | L_FORWARD | L_REVERSE);
+    P6OUT &= ~(RIGHT_FWD | RIGHT_REV | LEFT_FWD | LEFT_REV);
+    P6DIR |= (RIGHT_FWD | RIGHT_REV | LEFT_FWD | LEFT_REV);
 
-    P6OUT &= ~GRN_LED;
-    P6DIR |= GRN_LED;
+    P6OUT &= ~GREEN_LED;
+    P6DIR |= GREEN_LED;
 }
+
