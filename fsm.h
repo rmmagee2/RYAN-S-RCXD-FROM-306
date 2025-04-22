83ff649aeca086fc5d56967a18c3c43a8c64972b
rmmagee2 <130113674+rmmagee2@users.noreply.github.com>
2025-04-22 00:02:30 -0400

    Update fsm.h

--- fsm.h
+++ fsm.h
@@ -1,24 +1,26 @@
+#ifndef FSM_H
+#define FSM_H
+
 // fsm.h
 #include "defines.h"
 #include "timers.h"
 #include "defines.h"
 
-#ifndef FSM_H
-#define FSM_H
+void Set_Next_State(FSM_State new_state);
 
-typedef enum {
-    STATE_IDLE,
-    STATE_THUMBWHEEL_MENU,
-    STATE_CALIBRATION,
-    STATE_CONFIGURE_WHEELS,
-    STATE_FORWARD_START,
-    STATE_FORWARD_ADJUST,
-    STATE_REVERSE_START,
-    STATE_REVERSE_ADJUST,
-    STATE_INITIATE_STOP,
-    STATE_STOP,
-    STATE_PROCESS_COMMAND
-} FSM_State;
+//typedef enum {
+//    STATE_IDLE,
+//    STATE_THUMBWHEEL_MENU,
+//    STATE_CALIBRATION,
+//    STATE_CONFIGURE_WHEELS,
+//    STATE_FORWARD_START,
+//    STATE_FORWARD_ADJUST,
+//    STATE_REVERSE_START,
+//    STATE_REVERSE_ADJUST,
+//    STATE_INITIATE_STOP,
+//    STATE_STOP,
+//    STATE_PROCESS_COMMAND
+//} FSM_State;
 
 void FSM_Init(void);
 void FSM_Process(void);
