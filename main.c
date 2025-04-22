88ea5cada9e51f2e60161653fc1eeb4d595d5bf3
rmmagee2 <130113674+rmmagee2@users.noreply.github.com>
2025-04-22 00:04:44 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,13 +2,18 @@
 #include "fsm.h"
 #include "uart_comm.h"
 #include "sensors.h"
+#include "lcd.h"
 
 int main(void) {
     Init_All();
 
+    LCD_Print("Boot OK");
+
+    Set_Next_State(STATE_THUMBWHEEL_MENU);  // Force a state for debug
+
     while (1) {
-        UART_Process();
-        Update_Sensors();
-        FSM_Process();
+        UART_Process();      // Handle IOT commands
+        Update_Sensors();    // Poll line sensors
+        FSM_Process();       // Run main FSM
     }
 }
