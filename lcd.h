9dd423af5fb382fae83535b7ad9a64611eecaf40
rmmagee2 <130113674+rmmagee2@users.noreply.github.com>
2025-04-22 00:04:24 -0400

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -4,7 +4,8 @@
 void Init_LCD(void);
 
 void LCD_Clear(void);
-void LCD_Print(char* line1, char* line2, char* line3, char* line4);
+//void LCD_Print(char* line1, char* line2, char* line3, char* line4);
+void LCD_Print(const char* str);
 
 #endif // LCD_H_
 
