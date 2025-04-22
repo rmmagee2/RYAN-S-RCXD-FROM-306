c2bfee80fdb653432a73f2726174455494ce42fa
rmmagee2 <130113674+rmmagee2@users.noreply.github.com>
2025-04-22 00:00:46 -0400

    Update adc.h

--- adc.h
+++ adc.h
@@ -1,6 +1,8 @@
 #ifndef ADC_H_
 #define ADC_H_
 
+#include <stdint.h>
+
 #define ADC_IR_LEFT     0
 #define ADC_IR_RIGHT    1
 #define ADC_THUMB       2
@@ -10,20 +12,7 @@ extern volatile unsigned int adc_values[3];
 void Init_ADC(void);
 void Start_ADC(uint8_t channel);
 
-#define MODE_THRESHOLD_1 500
-#define MODE_THRESHOLD_2 1500
-#define MODE_THRESHOLD_3 2500
-#define MODE_THRESHOLD_4 3500
 
-typedef enum {
-    MODE_NONE,
-    MODE_START,
-    MODE_STOP,
-    MODE_MANUAL,
-    MODE_AUTO
-} Menu_Mode;
-
-Menu_Mode Thumbwheel_GetMode(void);
 void Thumbwheel_Menu(void);
 
 #endif /* ADC_H_ */
