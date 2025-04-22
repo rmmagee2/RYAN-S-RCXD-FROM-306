c13abb922c78138da7d0cfa1017635d591277f56
rmmagee2 <130113674+rmmagee2@users.noreply.github.com>
2025-04-22 00:02:10 -0400

    Update defines.h

--- defines.h
+++ defines.h
@@ -1,79 +1,81 @@
-#ifndef DEFINES_H_
-#define DEFINES_H_
+// fsm.c
+#include "fsm.h"
+#include "motors.h"
+#include "adc.h"
+#include "lcd.h"
+#include "sensors.h"
+#include "uart_comm.h"
+#include "defines.h"
+#include "timers.h"
+#include "lcd.h"
 
-// ================== CLOCK SPEEDS ==================
-#define SMCLK_FREQ     8000000     // SMCLK = 8MHz assumed from DCO config in clocks.c
-#define ACLK_FREQ       10000      // ACLK = ~10kHz from VLO (or 32768 Hz if LFXT is used)
+static FSM_State current_state = STATE_IDLE;
+static FSM_State next_state = STATE_IDLE;
 
-// ================== PWM ==================
-#define PWM_PERIOD      1000       // TimerB3 CCR0  8 kHz PWM if SMCLK is 8MHz (Frequency = 8MHz / 1000 = 8kHz PWM, not 1kHz as commented)
-#define FULL_SPEED      1000
-#define HALF_SPEED       500
-#define OFF                0
+void FSM_Init(void) {
+    current_state = STATE_IDLE;
+    next_state = STATE_IDLE;
+}
 
-// ================== ADC ==================
-#define ADC_MAX_VALUE   4095
-#define BLACK_THRESHOLD  500        // Calibrate this
-//#define BLACK_THRESHOLD ((white_avg + black_avg) / 2)
+void Set_Next_State(FSM_State new_state) {
+    next_state = new_state;
+}
 
-// ================== UART ==================
-#define UART_BUFFER_SIZE     64
-#define PROCESS_BUFFER_SIZE  32
+void FSM_Process(void) {
+    if (current_state != next_state) {
+        current_state = next_state;
+    }
 
-#define UART_PC        0
-#define UART_IOT       1
+    switch (current_state) {
+        case STATE_IDLE:
+            break;
+        
+        case STATE_CALIBRATION:
+            Calibrate_IR_Sensors();
+            break; 
+        
+        case STATE_THUMBWHEEL_MENU:
+            // process potentiometer values for menu selection
+            Thumbwheel_Menu();
+            break;
 
-// ================== STATE MACHINE ENUM (for later) ==================
-// defines.h (Add if unifying FSM states)
-typedef enum {
-  STATE_IDLE,
-  STATE_THUMBWHEEL_MENU,
-  STATE_CONFIGURE_WHEELS,
-  STATE_FORWARD_START,
-  STATE_FORWARD_ADJUST,
-  STATE_REVERSE_START,
-  STATE_REVERSE_ADJUST,
-  STATE_INITIATE_STOP,
-  STATE_CALIBRATION,
-  STATE_STOP,
-  STATE_PROCESS_COMMAND,
-  STATE_AUTO,
-  STATE_MANUAL
-} FSM_State;
+        case STATE_CONFIGURE_WHEELS:
+            Configure_Wheels();
+            break;
 
-// ================== EVENT ENUM (for future FSM logic) ==================
-typedef enum {
-  EVENT_NONE,
-  EVENT_IR_LEFT_HIGH,
-  EVENT_IR_RIGHT_LOW,
-  EVENT_MANUAL_LEFT_TURN,
-  EVENT_START_CMD,
-  EVENT_STOP_CMD,
-  EVENT_LINE_FOUND,
-  EVENT_TIMEOUT,
-  EVENT_MANUAL_CMD,
-  EVENT_AUTO_CMD
-} Event;
+        case STATE_FORWARD_START:
+            Move_Forward_Start();
+            break;
 
-extern Event current_event;
+        case STATE_FORWARD_ADJUST:
+            Adjust_Forward();
+            break;
 
-// ==================== MENU THUMB WHEEL ENUM ANd dEfiNES =================
-#define ADC_IR_LEFT  0
-#define ADC_IR_RIGHT 1
-#define ADC_THUMB    2
+        case STATE_REVERSE_START:
+            Move_Reverse_Start();
+            break;
 
-#define MODE_THRESHOLD_1 1000
-#define MODE_THRESHOLD_2 2000
-#define MODE_THRESHOLD_3 3000
+        case STATE_REVERSE_ADJUST:
+            Adjust_Reverse();
+            break;
 
-typedef enum {
-    MODE_START,
-    MODE_STOP,
-    MODE_MANUAL,
-    MODE_AUTO
-} Menu_Mode;
+        case STATE_INITIATE_STOP:
+            Stop_Movement_Init();
+            break;
 
-extern Menu_Mode Thumbwheel_GetMode(void);
-#endif // DEFINES_H_
+        case STATE_STOP:
+            Stop_Movement_Final();
+            break;
 
+        case STATE_PROCESS_COMMAND:
+            Process_IOT_Command();
+            break;
 
+        default:
+            break;
+    }
+}
+
+FSM_State Get_Current_State(void) {
+    return current_state;
+}
